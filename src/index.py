72ced9d5f2d5741f5036783592ae733634d178ac
raymond8121 <114804163+raymond8121@users.noreply.github.com>
2023-10-07 16:42:32 +0100

    Update index.py
    added sliders + buttons (dont press)

--- src/index.py
+++ src/index.py
@@ -1,20 +1,26 @@
 from ursina import *
 
 app = Ursina()
+globe = Entity(model='sphere', color=color.white, heightmap = 'Image')
 
-globe = Entity(model='sphere', color=color.white, scale_y=2)
+b = Button(text='Size', color=color.gray, scale=.10, text_origin=(0,0), origin = (8,-4))
+b.on_click = application.quit # assign a function to the button.
+b.tooltip = Tooltip('exit')
 
+slider = Slider(0, 20, default=10, height=Text.size*3, y=0.3, x=-0.85, step=1, horizontal=True)
 
-def update():
-    player.x += held_keys['d'] * time.dt
-    player.x -= held_keys['a'] * time.dt
+b = Button(text='Temperature', color=color.gray, scale=.10, text_origin=(0,0), origin = (8,-2))
+b.on_click = application.quit # assign a function to the button.
+b.tooltip = Tooltip('exit')
 
-def input(key):
-    if key == 'space':
-        player.y += 1
-        invoke(setattr, player, 'y', player.y-1, delay=.25)
+slider = Slider(0, 20, default=10, height=Text.size*3, y=0.1, x=-0.85, step=1, horizontal=True)
 
+b = Button(text='Gravity', color=color.gray, scale=.10, text_origin=(0,0), origin = (8,0))
+b.on_click = application.quit # assign a function to the button.
+b.tooltip = Tooltip('exit')
 
+slider = Slider(0, 20, default=10, height=Text.size*3, y=-.1, x=-0.85, step=1, horizontal=True)
+
+EditorCamera()
 # start running the game
 app.run()
-
