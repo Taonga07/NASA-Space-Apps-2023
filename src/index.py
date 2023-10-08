ab426bae3ef36b941710503008063530a70b276f
Raymond <raymond-8121@digilocal.org.uk>
2023-10-08 14:49:16 +0100

    added entities on planet

--- src/index.py
+++ src/index.py
@@ -1,26 +1,34 @@
 from ursina import *
 
+class set:
+    def __init__(self, pos_x:float, pos_y:float) -> None:
+        self.pos:list[float, float] = [pos_x, pos_y]
+        self.children: list[object, object] = self.create_set()
+    def create_set(self) -> list[object, object]:
+        button = Button(text='Size', color=color.gray, scale=.10, text_origin=(0,0), origin = self.pos)
+        slider = ThinSlider(0, 20, default=10, y=self.pos[1], x=self.pos[0], step=1, horizontal=True)
+        return [button, slider]
+    def hidden(self, hidden:bool) -> None:
+        for child in self.children:
+            child.enabled=hidden
+
+def minimize(b, item) -> None:
+    if b.text == "+":
+        b.text = "-"
+        [set.hidden(False) for set in sets]
+    if b.text == "-":
+        b.text = "+"
+        [set.hidden(True) for set in sets]
 app = Ursina()
-globe = Entity(model='sphere', color=color.white, heightmap = 'Image')
+globe = Entity(model='sphere', color=color.white)
+something = Entity(model='sphere', color=color.black,y=0.5,x=0.2,scale=0.1)
+something = Entity(model='sphere', color=color.black,y=-0.5,x=0.2,scale=0.1)
+button = Button(text='+', color=color.gray, scale=.05, text_origin=(0,0), origin = (17,-9))
+button.tooltip = Tooltip('minimise')
 
-b = Button(text='Size', color=color.gray, scale=.10, text_origin=(0,0), origin = (8,-4))
-b.on_click = application.quit # assign a function to the button.
-b.tooltip = Tooltip('exit')
-
-slider = Slider(0, 20, default=10, height=Text.size*3, y=0.3, x=-0.85, step=1, horizontal=True)
-
-b = Button(text='Temperature', color=color.gray, scale=.10, text_origin=(0,0), origin = (8,-2))
-b.on_click = application.quit # assign a function to the button.
-b.tooltip = Tooltip('exit')
-
-slider = Slider(0, 20, default=10, height=Text.size*3, y=0.1, x=-0.85, step=1, horizontal=True)
-
-b = Button(text='Gravity', color=color.gray, scale=.10, text_origin=(0,0), origin = (8,0))
-b.on_click = application.quit # assign a function to the button.
-b.tooltip = Tooltip('exit')
-
-slider = Slider(0, 20, default=10, height=Text.size*3, y=-.1, x=-0.85, step=1, horizontal=True)
-
+sets = [set(8,-3), set(8,-1)]
+def update():
+    button.on_click = minimize(button, sets)
 EditorCamera()
 # start running the game
 app.run()
